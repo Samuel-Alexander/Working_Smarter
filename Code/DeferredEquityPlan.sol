191796288a2cc64b78b6de87a3847d1a27dd6dcf
Sam Remy <samuel.a.remy@gmail.com>
2021-06-11 18:38:55 -0500

    Update DeferredEquityPlan.sol

--- Code/DeferredEquityPlan.sol
+++ Code/DeferredEquityPlan.sol
@@ -7,13 +7,14 @@ contract DeferredEquityPlan {
     address payable employee; // bob
     bool active = true; // this employee is active at the start of the contract
 
-    // @TODO: Set the total shares and annual distribution
-    // Your code here!
+    // Set the total shares and annual distribution
+    uint total_shares = 1000;
+    uint annual_distribution = 250;
 
     uint start_time = now; // permanently store the time this contract was initialized
 
-    // @TODO: Set the `unlock_time` to be 365 days from now
-    // Your code here!
+    // Set the `unlock_time` to be 365 days from now
+    uint unlock_time = now + 365;
 
     uint public distributed_shares; // starts at 0
 
@@ -26,17 +27,19 @@ contract DeferredEquityPlan {
         require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
         require(active == true, "Contract not active.");
 
-        // @TODO: Add "require" statements to enforce that:
+        // Add "require" statements to enforce that:
         // 1: `unlock_time` is less than or equal to `now`
+        require(unlock_time <= now, "Shares have not yet vested");
         // 2: `distributed_shares` is less than the `total_shares`
-        // Your code here!
+        require(distributed_shares < total_shares, "All shares have vested");
 
-        // @TODO: Add 365 days to the `unlock_time`
-        // Your code here!
+        // Add 365 days to the `unlock_time`
+        unlock_time + 365;
 
-        // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
-        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
-        // Your code here!
+        // Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
+        distributed_shares = (now - start_time) / 365 days * annual_distribution;
+
+        
 
         // double check in case the employee does not cash out until after 5+ years
         if (distributed_shares > 1000) {

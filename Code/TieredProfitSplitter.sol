90bd57e4c31ca7d21e9df2b20fdbb6928204f92d
Sam Remy <samuel.a.remy@gmail.com>
2021-06-11 18:39:46 -0500

    Update TieredProfitSplitter.sol

--- Code/TieredProfitSplitter.sol
+++ Code/TieredProfitSplitter.sol
@@ -22,13 +22,19 @@ contract TieredProfitSplitter {
         uint total;
         uint amount;
 
-        // @TODO: Calculate and transfer the distribution percentage
-        // Step 1: Set amount to equal `points` * the number of percentage points for this employee
-        // Step 2: Add the `amount` to `total` to keep a running total
-        // Step 3: Transfer the `amount` to the employee
+        // Calculate and transfer the distribution percentage
+        amount = points * 60;// Set amount to equal `points` * the number of percentage points for this employee
+        total += amount;// Add the `amount` to `total` to keep a running total
+        employee_one.transfer(amount);// Transfer the `amount` to the employee
 
-        // @TODO: Repeat the previous steps for `employee_two` and `employee_three`
-        // Your code here!
+        // Repeat the previous steps for `employee_two` and `employee_three`
+        amount = points * 25;
+        total += amount;
+        employee_two.transfer(amount);
+        
+        amount = points * 15;
+        total += amount;
+        employee_three.transfer(amount);
 
         employee_one.transfer(msg.value - total); // ceo gets the remaining wei
     }
